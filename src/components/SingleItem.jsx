ec8351891e6fa8cb23efa718da4c855a64f94392
sukirtib <sukirtibasnet967@gmail.com>
2026-02-21 13:49:05 +0545

    add delete feature

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,7 +1,7 @@
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 import "./SingleItem.css";
 
-const SingleItem = ({ item,  editCompleted }) => {
+const SingleItem = ({ item,  editCompleted, removeItem }) => {
   return (
     <div className="single-item">
       <input type="checkbox" checked={item.completed} onChange={() => editCompleted(item.id)} />
@@ -14,7 +14,9 @@ const SingleItem = ({ item,  editCompleted }) => {
         {item.name}
       </p>
 
-      <button className="btn icon-btn" type="button" onClick={() => null}>
+      <button className="btn icon-btn" 
+      type="button" 
+      onClick={() => removeItem(item.id)}>
         <FiEdit size={18} />
       </button>
 
