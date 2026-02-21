c62840c673a3bc22de3e0b0cfcb62f5f11fc4044
sukirtib <sukirtibasnet967@gmail.com>
2026-02-21 13:40:01 +0545

    add edit feature

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,10 +1,10 @@
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 import "./SingleItem.css";
 
-const SingleItem = ({ item }) => {
+const SingleItem = ({ item,  editCompleted }) => {
   return (
     <div className="single-item">
-      <input type="checkbox" checked={item.completed} onChange={() => null} />
+      <input type="checkbox" checked={item.completed} onChange={() => editCompleted(item.id)} />
       <p
         style={{
           textTransform: "capitalize",
